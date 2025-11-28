7f36ea9355f4750286641931ca233f41de8fd605
juneh2633 <juneh2633@gmail.com>
2025-11-28 17:49:55 +0900

    .

--- lib/model/tournament.ts
+++ lib/model/tournament.ts
@@ -1,5 +1,11 @@
-export 
-interface TournamentData {
+import { OvertimeData } from "./overtimeData"
+import { RoundData } from "./round"
+import { Team } from "./team"
+import { SongInfo } from "./songInfo"
+
+export type SongInfoMap = Record<string, SongInfo>
+
+export interface TournamentData {
   team1: Team
   team2: Team
   rounds: [RoundData, RoundData, RoundData, RoundData]
